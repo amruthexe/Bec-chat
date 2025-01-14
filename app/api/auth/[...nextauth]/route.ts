3ccaff4176eb440e8ddee6f15c86ac329c844962
amruthexe <aluriamruthrajit@gmail.com>
2025-01-14 18:26:07 +0530

    auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,6 @@
+import bcrypt from 'bcrypt';
 import NextAuth, { AuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
 import GoogleProvider from 'next-auth/providers/google';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 
@@ -7,6 +9,42 @@ import prisma from '@/app/libs/prismadb';
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        // check if user details are passed
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('Please enter your credentials');
+        }
+
+        // find user in database
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        // check if user exists / hashed password is only there for non-social logins
+        if (!user || !user?.hashedPassword) {
+          throw new Error('Invalid credentials');
+        }
+
+        // compare passwords
+        const isPasswordCorrect = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        if (!isPasswordCorrect) {
+          throw new Error('Invalid credentials');
+        }
+
+        // if all checks pass, return user
+        return user;
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID as string,
       clientSecret: process.env.GOOGLE_SECRET as string,
