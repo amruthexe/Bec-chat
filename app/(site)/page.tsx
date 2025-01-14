9be2512f914fda8a7f7b1a90c9353db352c46d7f
amruthexe <aluriamruthrajit@gmail.com>
2025-01-14 18:35:07 +0530

    fixed mobile

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,34 +3,33 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex flex-col-reverse lg:flex-row min-h-screen">
+    <main className="flex flex-col lg:flex-row min-h-screen">
       {/* Left Column (CTA) */}
       <div
-        className="lg:w-1/2 p-10"
-        style={{ background: 'linear-gradient(45deg, #00BFFF, #0099CC)' }}
+        className="lg:w-1/2 flex flex-col justify-center p-10"
+        style={{ background: 'linear-gradient(45deg, #00BFFF, #0099CC)', minHeight: '50vh' }}
       >
-        <div className="flex flex-col justify-center h-full">
+        <div className="flex flex-col justify-center items-center h-full">
           <Image
             alt="logo"
-            className="mx-auto w-auto bg-white rounded-full"
+            className="w-auto bg-white rounded-full"
             height={100}
             width={100}
             src="/images/logo.png"
           />
           
-          <h1 className="text-4xl font-bold mb-2 pt-8 text-center text-white">
+          <h1 className="text-3xl lg:text-4xl font-bold mb-2 pt-8 text-center text-white">
             🚀 Welcome to Bec Alumni Chat! 🎉
           </h1>
-          <p className="text-lg my-6 mx-auto text-center text-white">
-          Your Ultimate Chat Experience with <span className='font-bold text-2xl'>BEC Alumni</span> 🎓
+          <p className="text-md lg:text-lg my-6 text-center text-white">
+            Your Ultimate Chat Experience with <span className="font-bold text-xl lg:text-2xl">BEC Alumni</span> 🎓
           </p>
-          
         </div>
       </div>
 
       {/* Right Column (Form) */}
-      <div className="lg:w-1/2 bg-gray-100 p-10">
-        <div className="sm:mx-auto flex flex-col justify-center h-full sm:w-full sm:max-w-md">
+      <div className="lg:w-1/2 bg-gray-100 p-8 lg:p-10 flex items-center justify-center">
+        <div className="sm:w-full sm:max-w-md">
           <Image
             alt="logo"
             className="mx-auto w-auto"
@@ -39,8 +38,8 @@ export default function Home() {
             src="/images/logo.png"
           />
 
-          <h2 className="mt-6 text-center text-3xl font-bold text-gray-900 tracking-tight">
-            🚀 Join Bec Alumni Chat Today! 
+          <h2 className="mt-6 text-center text-2xl lg:text-3xl font-bold text-gray-900">
+            🚀 Join Bec Alumni Chat Today!
           </h2>
 
           {/* Auth Form */}
